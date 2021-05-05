86b877564c5af1e2cbd68e1269b80a68b53599c8
Denisario <KROTIK96@gmail.com>
2021-05-06 00:18:53 +0300

    Added credits(need to rework)

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -2,12 +2,14 @@ import React from 'react'
 import {Switch, Route} from 'react-router-dom'
 import Finances from '../pages/Finances';
 import Finance from '../pages/Finance';
+import Credit from '../pages/Credit';
 
 const AppRouter = () => {
     return (        
         <Switch>
                 <Route path="/finances" component={Finances} exact/>
                 <Route path="/finance/:id" component={Finance} exact/>
+                <Route path="/credit" component={Credit} exact/>
         </Switch>
     )
 }
